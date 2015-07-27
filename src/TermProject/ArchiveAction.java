54d4fc70dccdbb6dca288508391d90b6acd12d5f
seanremy <seanremy@gmail.com>
2015-07-26 23:22:38 -0400

    Update ArchiveAction.java
    Fixed archivePage() to be List and added getPageTitle()

--- src/TermProject/ArchiveAction.java
+++ src/TermProject/ArchiveAction.java
@@ -1,24 +1,24 @@
 package TermProject;
 
 import java.util.Map;
-import java.util.Vector;
+import java.util.List;
 
 import org.apache.struts2.interceptor.ParameterAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ArchiveAction extends ActionSupport implements ParameterAware {
+public class ArchiveAction extends BaseAction implements ParameterAware {
 
 	static int page;
 	
 	//get archive page number p. p = 0 gives the 5 most recent posts, p = 1 gives the next 5, etc.
-	public Post[] archivePage() {
+	public List<Post> archivePage() {
 		
 		Post[] posts = new Post[5];
-		Vector<Integer> postIDs = Post.getPostIDs();
+		List<Integer> postIDs = Post.getPostIDs();
 		
 		for(int i = 0; i < postIDs.size() && 5*page + i < postIDs.size(); i++) {
-			posts[i] = Post.getPostByID(postIDs.elementAt(postIDs.size() - (5 * page + i + 1)));
+			posts.add(Post.getPostByID(postIDs.elementAt(postIDs.size() - (5 * page + i + 1)));
 		}
 		
 		return posts;
@@ -34,6 +34,10 @@ public class ArchiveAction extends ActionSupport implements ParameterAware {
 	//only parameter to be passed is called Page_Number
 	public void setParameters(Map<String, String[]> map) {
 		page = Integer.parseInt(map.get("Page_Number")[0]);
+	}
+	
+	public String getPageTitle() {
+		return "Archive";
 	}
 	
 }
