750ae1f82cbd0a4b289a0c5debe1c06bd0ac75fb
seanremy <seanremy@gmail.com>
2015-07-26 21:29:29 -0400

    Update CreatePostAction.java
    Added createPost()

--- src/TermProject/CreatePostAction.java
+++ src/TermProject/CreatePostAction.java
@@ -1,20 +1,19 @@
 package TermProject;
 
 import java.util.Map;
+import java.util.Date;
+import java.sql.Timestamp;
 
 import org.apache.struts2.interceptor.ParameterAware;
 import com.opensymphony.xwork2.ActionSupport;
 
-
-public class CreatePostAction extends ActionSupport implements ParameterAware {
+public class CreatePostAction extends BaseAction {
 	
 	private String text;
 	private String title;
-	private String postTime;
+	private Timestamp postTime;
 	private Integer userId;
 	private Integer PostId;
-	
-	
 	
 	public Integer getUserId() {
 		return userId;
@@ -54,43 +53,31 @@ public class CreatePostAction extends ActionSupport implements ParameterAware {
 		return PostId;
 	}
 	
-	public String getPostTime()
+	public Timestamp getPostTime()
 	{
 		return postTime;
 	}
 	
-	public void setPostTime()//generates a timestamp for postTime
+	public void setTimestamp(Timestamp t)
 	{
-		
-		java.sql.Date D= new java.sql.Date(System.currentTimeMillis());
-		postTime=D.toString();
-		
+		postTime = t;	
 	}
 	
-	
-	public Post Post()
+	public void createPost()
 	{
-		Integer i=null;
-		Post P= new Post();
-		P.setPostText(getText());
-		P.setPostTitle(getTitle());
-		P.setUserID(getUserId());
-		P.setPostID(i);
-		setPostTime();
-		P.setTimeCommented(getPostTime());
 		
-		P.save();
+		Date today = new Date();
+		pt = new Timestamp(today.getTime());
+		Post post = new Post(null, getTitle(), getText(), getCurrentUser().getID(), pt);
+		post.save();
 		
-		return P;
 	}
 	
 	@Override
 	public void setParameters(Map<String, String[]> map) {
 		
-		userId = Integer.parseInt(map.get("User_Id")[0]);
 		text = map.get("Text")[0];
 		title=map.get("Post_Title")[0];
-
 		
 	}
 	
